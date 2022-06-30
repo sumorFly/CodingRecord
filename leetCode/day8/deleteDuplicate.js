6cda37a0d6dcf9e5ee882fa07e3cc72496e12b23
xieyanfei <xieyanfei>
2022-06-30 11:16:56 +0800

    2022-06-30 第八天

---
+++ leetCode/day8/deleteDuplicate.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+//  给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if (head == null || head.next == null) return head
+    let cur = head;
+    while (cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
+
+};
